8a9e04ba7aa6c1dd4932ec5b971c306a1816bf67
EC2 Default User <ec2-user@ip-172-31-17-50.us-east-2.compute.internal>
2020-12-05 23:27:47 +0000

    typescript to js

---
+++ src/components/CorpInfo.js
@@ -0,0 +1,10 @@
+const CorpInfo = () => (
+    <div id="corpInfo">
+        <div>
+            <h1>Formul[ai]c</h1>
+            <p>Cloud Solutions</p>
+        </div>
+    </div>
+);
+
+export default CorpInfo;
